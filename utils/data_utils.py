f82853186a9ef6982aa2714130e5f83602841d75
ouroboros_csl@163.com <uroboros@MSI.localdomain>
2023-01-21 22:30:34 +0800

    add option_utils

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
+import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
 
 def load_data(data_dir, dataset='wiki'):
     if not isinstance(data_dir, Path):
@@ -8,12 +12,12 @@ def load_data(data_dir, dataset='wiki'):
         
     if dataset == 'wiki':
         G = nx.Graph()
-        with open(data_dir / 'wiki\Wiki_edgelist.txt', 'r', encoding='utf-8') as f:
+        with open(data_dir / 'wiki/Wiki_edgelist.txt', 'r', encoding='utf-8') as f:
             lines = f.readlines()
             edge_list = [tuple(edge_info.strip().split(' ')) for edge_info in lines]
             G.add_edges_from(edge_list)
         
-        with open(data_dir / 'wiki\Wiki_labels.txt', 'r', encoding='utf-8') as f:
+        with open(data_dir / 'wiki/wiki_labels.txt', 'r', encoding='utf-8') as f:
             lines = f.readlines()
             for label_info in lines:
                 node, label = label_info.strip().split(' ')
@@ -24,7 +28,27 @@ def load_data(data_dir, dataset='wiki'):
     elif dataset == 'cora':
         pass
     
-    return G      
+    return G
+
+def visualize_embeddings(embedding_dict, save_path, title:str=None, colors=None):
+    
+    print("INFO: Visualize embeddings...")
+    embeddings = np.asarray(list(embedding_dict.values()))
+    tsne = TSNE(n_components=2, init='pca', random_state=0)
+    embedding_pos = tsne.fit_transform(embeddings)
+    
+    xs, ys = [], []
+    for x, y in embedding_pos:
+        xs.append(x)
+        ys.append(y)
+    
+    plt.figure(figsize=(10, 10)) 
+    plt.scatter(xs, ys, s=1, c=colors)
+    plt.title(title)
+    plt.savefig(save_path)
+    
+    
+        
             
                 
                 
