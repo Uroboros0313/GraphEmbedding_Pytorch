44fdee868142492a1ce257f6b87c7df82942fbc2
Uroboros0313 <ouroboros_csl@163.com>
2023-02-04 18:36:23 +0800

    add jdata reader

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,5 +1,8 @@
+import os
+import pickle as pkl
 from pathlib import Path
 
+import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -9,7 +12,8 @@ from sklearn.manifold import TSNE
 def load_data(data_dir, dataset='wiki'):
     if not isinstance(data_dir, Path):
         data_dir = Path(data_dir)
-        
+    
+    ################################# wiki数据集预处理 ###################################
     if dataset == 'wiki':
         G = nx.Graph()
         with open(data_dir / 'wiki/Wiki_edgelist.txt', 'r', encoding='utf-8') as f:
@@ -22,7 +26,8 @@ def load_data(data_dir, dataset='wiki'):
             for label_info in lines:
                 node, label = label_info.strip().split(' ')
                 G.nodes[node]['label'] = label
-
+    
+    ################################# cosearch数据集预处理 ###################################
     elif dataset == 'cosearch':
         G = nx.Graph()
         with open(data_dir / 'cosearch/co_search_2016.txt', 'r', encoding='utf-8') as f:
@@ -32,10 +37,57 @@ def load_data(data_dir, dataset='wiki'):
                 sc, tg, w = edge_info.strip().split(' ')
                 edge_list.append((sc, tg, int(w)))
             G.add_weighted_edges_from(edge_list)
-              
-    elif dataset == 'jdata':
-        pass
+            
+    ################################# jdata数据集预处理 ###################################          
     
+    elif dataset == 'jdata':
+        tmp_path = data_dir / 'tmp/jdata/jdata_graph.pkl'
+        if os.path.exists(tmp_path):
+            with open(tmp_path, 'rb') as f:
+                G = pkl.load(f)
+        else:
+            window_size = 5
+            G = nx.Graph()
+
+            user_actions = pd.read_csv(data_dir/'jdata/action_head.csv', parse_dates=['action_time'], header=0)
+            user_actions = user_actions[['user_id', 'sku_id', 'action_time']]
+
+            items = pd.read_csv(data_dir/'jdata/jdata_product.csv', header=0)
+            items = items[['sku_id','brand','shop_id','cate']]
+
+            user_actions = user_actions.merge(items, on='sku_id', how='inner')
+            items = user_actions[['sku_id','brand','shop_id','cate']].drop_duplicates()
+            user_actions = user_actions[['user_id', 'sku_id']]
+
+            sessions = user_actions.groupby('user_id').agg(list)
+            for vis_info in sessions.itertuples():
+                sess = vis_info[1]
+                if len(sess) == 1:
+                    G.add_node(sess[0])
+                    continue
+
+                for i in range(len(sess)):
+                    for j in range(max(0, i - window_size), min(len(sess), i + window_size)):
+                        if i==j:
+                            continue
+                        t, v = sess[i], sess[j]
+                        if t not in G.nodes or v not in G.nodes:
+                            G.add_edge(t, v, weight=1)
+                        elif v not in G[t]:
+                            G.add_edge(t, v, weight=1)
+                        else:
+                            G[t][v]['weight'] += 1
+                            
+            for node_info in items.itertuples():
+                G.nodes[node_info.sku_id]['brand'] = node_info.brand
+                G.nodes[node_info.sku_id]['shop_id'] = node_info.shop_id
+                G.nodes[node_info.sku_id]['cate'] = node_info.cate
+            
+            os.mkdir(tmp_path.parent)
+            with open(tmp_path, 'wb') as f:
+                pkl.dump(G, f)
+                
+    ################################# cora数据集预处理 ###################################
     elif dataset == 'cora':
         G = nx.Graph()
         with open(data_dir / 'cora/cora.cites', 'r', encoding='utf-8') as f:
